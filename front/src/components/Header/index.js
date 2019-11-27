c56772597424399eca0b185670f8ea9d1387d93c
JulianCamart <Julian.camart@gmail.com>
2019-11-27 20:26:30 +0100

    Change on header components

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons'
-import {Navbar} from 'rbx';
+import { Navbar, Column } from 'rbx';
 
 import "rbx/index.css";
 
@@ -9,25 +9,21 @@ import './style.sass';
 
 const Header = () => {
     return <header>
-
-        {/* <Column  desktop={ narrow= boolean, size=12 }>
-          
-        </Column> */}
-        <nav role="navigation">
-            <div id="menuToggle">
-                <input type="checkbox" />
-                <span></span>
-                <span></span>
-                <span></span>
-                <ul id="menu">
-                    <a href="#"><li>Home</li></a>
-                    <a href="#"><li>About</li></a>
-                    <a href="#"><li>Info</li></a>
-                    <a href="#"><li>Contact</li></a>
-                    <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
-                </ul>
-            </div>
-        </nav>
+            <nav role="navigation">
+                <div id="menuToggle">
+                    <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <ul id="menu">
+                        <a href="#"><li>Home</li></a>
+                        <a href="#"><li>About</li></a>
+                        <a href="#"><li>Info</li></a>
+                        <a href="#"><li>Contact</li></a>
+                        <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+                    </ul>
+                </div>
+            </nav>
     </header>
 }
 
