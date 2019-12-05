14bb42b0da2316ef312022815e73c687c161dbd2
BriceVortex <brice.zuninomourlon@gmail.com>
2019-12-05 18:38:59 +0100

    config return json

--- back/src/Controller/LoginController.php
+++ back/src/Controller/LoginController.php
@@ -5,24 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
-
 /**
- * @Route("/api", name="api_", methods={"POST"})
+ * @Route("/api", name="api_")
  */
-
 class LoginController extends AbstractController
 {
-
-
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="login", methods={"POST"})
      */
     public function index()
-    {
-
+    {  
         return $this->json(['result' => true]);
-
     }
 }
