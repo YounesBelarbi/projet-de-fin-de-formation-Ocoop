60540ee5381041ea95e5807aeab51d534639fad6
younes <y>
2019-12-06 12:17:58 +0100

    logout done

---
+++ back/src/Controller/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController extends AbstractController
+{
+    /**
+     * @Route("api/logout", name="api_logout")
+     */
+    public function logout()
+    {
+
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+
+    }
+}
