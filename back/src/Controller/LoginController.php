5ef13df5f29cffe6b7dd40e489c2f3276f352700
younes <ybelarbi@outlook.com>
2019-12-10 15:59:43 +0100

    token generation works

--- back/src/Controller/LoginController.php
+++ back/src/Controller/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 /**
  * @Route("/user", name="api_")
@@ -13,11 +17,14 @@ class LoginController extends AbstractController
     /**
      * @Route("/login", name="login", methods={"POST"})
      */
-    public function index()
+    public function index(UserInterface $user, JWTTokenManagerInterface $JWTManager)
     {  
+        
+        $token = new JsonResponse(['token' => $JWTManager->create($user)]);
 
+        return $this->json(['result' => true, 'user' => $this->getUser(), 'token' => $token]);
 
-        return $this->json(['result' => true, 'user' => $this->getUser()]);
+     
 
         
 
