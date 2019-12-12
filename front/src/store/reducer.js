287eb4cef2647230044ef27941ad9a6321e4a7d8
JulianCamart <Julian.camart@gmail.com>
2019-12-12 14:19:19 +0100

    Add new headerDashboardReducer to the combineReducer

--- front/src/store/reducer.js
+++ front/src/store/reducer.js
@@ -3,9 +3,12 @@ import { combineReducers } from 'redux';
 import registerReducer from 'src/store/register';
 import loginReducer from 'src/store/login';
 import dashboardReducer from 'src/store/dashboard';
+import headerDashboardReducer from 'src/store/headerDashboard';
+
 
 export default combineReducers({
   registerReducer,
   loginReducer,
-  dashboardReducer
+  dashboardReducer,
+  headerDashboardReducer
 });
