4600328646d1f2fd8d19704a90cb11c0ae0f5a8d
JulianCamart <Julian.camart@gmail.com>
2019-12-16 14:58:07 +0100

    Ajout d'un groups

--- back/src/Entity/Game.php
+++ back/src/Entity/Game.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
@@ -15,26 +16,32 @@ class Game
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups("game_information")
+     * 
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("game_information")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Groups("game_information")
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("game_information")
      */
     private $poster;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("game_information")
      */
     private $logo;
 
